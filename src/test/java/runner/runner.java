c56c97c0a331dec754a697c11184dc17f890aa07
Matías Pírez <matiaspirez26@gmail.com>
2021-03-22 09:46:10 -0300

    Add configuration to use Extent Reports

--- src/test/java/runner/runner.java
+++ src/test/java/runner/runner.java
@@ -10,7 +10,8 @@ import pages.BasePage;
 @CucumberOptions(
     features = "src/test/resources/features",
     monochrome = true,
-    glue = "steps"
+    glue = "steps",
+    plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "json:target/cucumber-reports.json"}
 )
 
 public class runner{
